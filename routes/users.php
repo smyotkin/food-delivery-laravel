d31579b1174b7e5309e28b2fc93e58ec6e5fc9eb
smyotkin <smyotkin@gmail.com>
2021-06-09 13:34:28 +0300

    Changed: Route group middleware.last_page move to auth group

--- routes/users.php
+++ routes/users.php
@@ -6,26 +6,27 @@ use App\Http\Controllers\Profile\ProfileController;
 Route::middleware(['auth'])->group(function () {
     Route::get('/users/getAjax', [UsersController::class, 'getAjax'])
         ->name('users/getAjax');
+
+    Route::middleware(['last.page'])->group(function () {
+        Route::get('/users', [UsersController::class, 'index'])
+            ->name('users');
+
+        Route::get('/users/create', [UsersController::class, 'create'])
+            ->name('users/create');
+
+        Route::post('/users', [UsersController::class, 'store'])
+            ->name('users/store');
+
+        Route::get('/users/{id}', [UsersController::class, 'show'])
+            ->whereNumber('id')
+            ->name('user');
+
+        Route::post('/users/{id}', [UsersController::class, 'update'])
+            ->name('users/update');
+
+        Route::get('/profile', [ProfileController::class, 'index'])
+            ->name('profile');
+    });
 });
 
-Route::middleware(['auth', 'last.page'])->group(function () {
-    Route::get('/users', [UsersController::class, 'index'])
-        ->name('users');
-
-    Route::get('/users/create', [UsersController::class, 'create'])
-        ->name('users/create');
-
-    Route::post('/users', [UsersController::class, 'store'])
-        ->name('users/store');
-
-    Route::get('/users/{id}', [UsersController::class, 'show'])
-        ->whereNumber('id')
-        ->name('user');
-
-    Route::post('/users/{id}', [UsersController::class, 'update'])
-        ->name('users/update');
-
-    Route::get('/profile', [ProfileController::class, 'index'])
-        ->name('profile');
-});
 
