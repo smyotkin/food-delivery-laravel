27eb76f7bd4747c091bb9aad66b04eebc51370a6
smyotkin <smyotkin@gmail.com>
2021-06-16 14:14:16 +0300

    Changed: Permissions table correct checked values

--- resources/views/users/permissions-table.blade.php
+++ resources/views/users/permissions-table.blade.php
@@ -25,10 +25,13 @@
 
                 <tr class="bg-light">
                     <td class="text-center">
-                        <input class="form-check-input permission" type="checkbox" name="permissions[]" value="{{ $permission->slug }}" id="{{ $permission->slug }}" {{ isset($role_permissions) && in_array($permission->slug, $role_permissions) ? 'checked' : '' }}>
+                        @php ($checkUserPermissions = !empty($user_permissions) && in_array($permission->id, $user_permissions))
+                        @php ($checkRolePermissions = !empty($role_permissions) && in_array($permission->slug, $role_permissions))
+
+                        <input class="form-check-input permission" type="checkbox" name="permissions[]" value="{{ $permission->slug }}" id="{{ $permission->slug }}" {{ $checkUserPermissions || (empty($user_permissions) && $checkRolePermissions) ? 'checked' : '' }}>
                     </td>
                     <td>
-                        <label class="form-check-label" for="{{ $permission->slug }}">
+                        <label class="form-check-label {{ isset($role_permissions) && in_array($permission->slug, $role_permissions) ? 'bg-secondary text-white px-2' : '' }}" for="{{ $permission->slug }}">
                             {{ $permission->name }}
                         </label>
                     </td>
