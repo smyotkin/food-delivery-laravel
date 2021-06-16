579ccc0c1db7c56aaf7f547eb3eb3497af9165eb
smyotkin <smyotkin@gmail.com>
2021-06-16 14:17:24 +0300

    Changed: User table(index) added view position

--- resources/views/users/users-table.blade.php
+++ resources/views/users/users-table.blade.php
@@ -1,6 +1,7 @@
 @if(!empty($data) && $data->count())
     @foreach($data as $user)
         @php ($position = isset($roles) && isset($roles->keyBy('id')[$user->position_id]) ? $roles->keyBy('id')[$user->position_id] : null)
+
         <tr class="{{ $user->online == 'online' ? 'fw-bold' : '' }} {{ $user->is_active == 0 ? 'text-muted' : '' }}" data-page="{{ $data->currentPage() }}">
             <td>
                 <a href="users/{{ $user->id }}" class="text-decoration-none {{ $user->is_active == 0 ? 'link-secondary' : '' }}">{{ $user->full_name }}</a>
