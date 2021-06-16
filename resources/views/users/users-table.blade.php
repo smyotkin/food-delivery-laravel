4d590c79eff2eb7b5f14dbfaf18789cd40957fd7
smyotkin <smyotkin@gmail.com>
2021-06-16 12:07:52 +0300

    Changed: Users index return positions

--- resources/views/users/users-table.blade.php
+++ resources/views/users/users-table.blade.php
@@ -1,11 +1,12 @@
 @if(!empty($data) && $data->count())
     @foreach($data as $user)
+        @php ($position = isset($roles) && isset($roles->keyBy('id')[$user->position_id]) ? $roles->keyBy('id')[$user->position_id] : null)
         <tr class="{{ $user->online == 'online' ? 'fw-bold' : '' }} {{ $user->is_active == 0 ? 'text-muted' : '' }}" data-page="{{ $data->currentPage() }}">
             <td>
                 <a href="users/{{ $user->id }}" class="text-decoration-none {{ $user->is_active == 0 ? 'link-secondary' : '' }}">{{ $user->full_name }}</a>
             </td>
             <td>{{ $user->phone_formatted }}</td>
-            <td>-</td>
+            <td>{{ $position->name ?? '---' }}</td>
             <td>{{ $user->registered_at }}</td>
             <td>{{ $user->last_page }}</td>
             <td class="{{ $user->online == 'online' ? 'text-success' : '' }}">
