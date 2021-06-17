bb189d2c22937dd9d01580f250d90e985a94044a
smyotkin <smyotkin@gmail.com>
2021-06-17 12:34:20 +0300

    Added: migrations column is_custom_permissions into users table

---
+++ database/migrations/2021_06_17_123048_add_is_custom_permissions_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsCustomPermissionsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('is_custom_permissions')->default(0)->unsigned()->after('is_active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
