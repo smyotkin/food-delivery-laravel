d4c0aa41444347ed9ebf5f704b784743af08f13c
smyotkin <smyotkin@gmail.com>
2021-06-21 09:24:10 +0300

    Refactoring: modify html

--- resources/views/users/position.blade.php
+++ resources/views/users/position.blade.php
@@ -45,104 +45,107 @@
                 <x-auth-validation-errors class="mb-4" :errors="$errors" />
             </div>
         </div>
-        <form method="post" action="{{ isset($role) ? route('positions.update', ['position' => $role]) : route('positions.store') }}" id="position_form" class="update_position">
-            @method(isset($role) ? 'patch' : 'post')
-            @csrf
 
-            <div class="row g-3">
-                <div class="col-4">
-                    <div class="row g-3">
-                        @if (isset($role))
-                            <input type="hidden" name="id" value="{{ $role->id }}">
-                        @endif
+        <div class="row">
+            <form method="post" action="{{ isset($role) ? route('positions.update', ['position' => $role]) : route('positions.store') }}" id="position_form" class="col-10 update_position">
+                @method(isset($role) ? 'patch' : 'post')
+                @csrf
 
-                        <div class="col-12">
-                            <label for="name" class="form-label fw-bold">Название</label>
-                            <input type="text" class="form-control rounded-0" id="name" name="name" value="{{ $role->name ?? '' }}" placeholder="Название">
-                        </div>
+                <div class="row g-3">
+                    <div class="col-5">
+                        <div class="row g-3">
+                            @if (isset($role))
+                                <input type="hidden" name="id" value="{{ $role->id }}">
+                            @endif
 
-                        <div class="col-12">
-                            <label for="slug" class="form-label fw-bold">Метка</label>
-                            <input type="text" class="form-control rounded-0" id="slug" name="slug" value="{{ $role->slug ?? '' }}" placeholder="Метка (латиницей с нижним подчеркиванием)">
-                        </div>
+                            <div class="col-12">
+                                <label for="name" class="form-label fw-bold">Название</label>
+                                <input type="text" class="form-control rounded-0" id="name" name="name" value="{{ $role->name ?? '' }}" placeholder="Название">
+                            </div>
 
-                        <div class="col-12">
-                            <label for="status" class="form-label fw-bold">Статус</label>
-                            <select class="form-select" id="status" name="status" required>
-                                <option disabled selected>Ничего не выбрано</option>
-                                @foreach ($statuses as $key => $status)
-                                    <option value="{{ $key }}" {{ isset($role->status) && $role->status == $key ? 'selected' : '' }}>{{ $status['name'] }}</option>
-                                @endforeach
-                            </select>
+                            <div class="col-12">
+                                <label for="slug" class="form-label fw-bold">Метка</label>
+                                <input type="text" class="form-control rounded-0" id="slug" name="slug" value="{{ $role->slug ?? '' }}" placeholder="Метка (латиницей с нижним подчеркиванием)">
+                            </div>
+
+                            <div class="col-12">
+                                <label for="status" class="form-label fw-bold">Статус</label>
+                                <select class="form-select" id="status" name="status" required>
+                                    <option disabled selected>Ничего не выбрано</option>
+                                    @foreach ($statuses as $key => $status)
+                                        <option value="{{ $key }}" {{ isset($role->status) && $role->status == $key ? 'selected' : '' }}>{{ $status['name'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="col-6">
-                    <div class="row g-3">
-                        <div class="col-12">
-                            <label class="form-label fw-bold">Права</label>
+                    <div class="col-7">
+                        <div class="row g-3">
+                            <div class="col-12">
+                                <label class="form-label fw-bold">Права</label>
 
-                            <table class="table table-sm align-middle">
-                                <thead>
-                                    <tr class="bg-lightgray">
-                                        <th class="text-start px-4" scope="col"></th>
-                                        <th scope="col">Название</th>
-                                        <th scope="col">Метка</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @php ($previousGroupValue = '')
+                                <table class="table table-sm align-middle">
+                                    <thead>
+                                        <tr class="bg-lightgray">
+                                            <th class="text-start px-4" scope="col"></th>
+                                            <th scope="col">Название</th>
+                                            <th scope="col">Метка</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @php ($previousGroupValue = '')
 
-                                    @foreach ($permissions as $permission)
-                                        @if ($permission->group != $previousGroupValue)
-                                            <tr>
-                                                <td></td>
-                                                <td colspan="2" class="py-3">
-                                                    <strong>{{ $permission->group }}</strong>
+                                        @foreach ($permissions as $permission)
+                                            @if ($permission->group != $previousGroupValue)
+                                                <tr>
+                                                    <td></td>
+                                                    <td colspan="2" class="py-3">
+                                                        <strong>{{ $permission->group }}</strong>
+                                                    </td>
+                                                </tr>
+                                            @endif
+
+                                            <tr class="bg-light">
+                                                <td class="text-center">
+                                                    <input class="form-check-input permission" type="checkbox" name="permissions[]" value="{{ $permission->slug }}" id="{{ $permission->slug }}" {{ isset($role_permissions) && in_array($permission->slug, $role_permissions) ? 'checked' : '' }}>
+                                                </td>
+                                                <td>
+                                                    <label class="form-check-label" for="{{ $permission->slug }}">
+                                                        {{ $permission->name }}
+                                                    </label>
+                                                </td>
+                                                <td>
+                                                    {{ $permission->slug }}
                                                 </td>
                                             </tr>
-                                        @endif
 
-                                        <tr class="bg-light">
-                                            <td class="text-center">
-                                                <input class="form-check-input permission" type="checkbox" name="permissions[]" value="{{ $permission->slug }}" id="{{ $permission->slug }}" {{ isset($role_permissions) && in_array($permission->slug, $role_permissions) ? 'checked' : '' }}>
-                                            </td>
-                                            <td>
-                                                <label class="form-check-label" for="{{ $permission->slug }}">
-                                                    {{ $permission->name }}
-                                                </label>
-                                            </td>
-                                            <td>
-                                                {{ $permission->slug }}
-                                            </td>
-                                        </tr>
-
-                                        @php ($previousGroupValue = $permission->group)
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                            @php ($previousGroupValue = $permission->group)
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </form>
 
-                @if (isset($role))
-                    <div class="col-2">
-                        <label class="form-label fw-bold">Управление</label>
+            @if (isset($role))
+                <div class="col-2">
+                    <label class="form-label fw-bold">Управление</label>
 
-                        <div class="text-center">
-                            @permission('users_position_delete')
-                                <form action="{{ route('positions.destroy', ['position' => $role->id]) }}" method="post">
-                                    @method('delete')
-                                    @csrf
+                    <div class="text-center">
+                        @permission('users_position_delete')
+                            <form action="{{ route('positions.destroy', ['position' => $role->id]) }}" method="post">
+                                @method('delete')
+                                @csrf
 
-                                    <button onclick="return confirm('Удалить?')" class="btn btn-sm btn-danger">Удалить должность</button>
-                                </form>
-                            @endpermission
-                        </div>
+                                <button onclick="return confirm('Удалить?')" class="btn btn-sm btn-danger w-100 py-2">Удалить должность</button>
+                            </form>
+                        @endpermission
                     </div>
-                @endif
-            </div>
-        </form>
+                </div>
+            @endif
+        </div>
     </div>
 </x-app-layout>
