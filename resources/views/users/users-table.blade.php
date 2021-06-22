a9be7dee83ff965a2b2d521055500da9e47cbdf1
smyotkin <smyotkin@gmail.com>
2021-06-22 09:31:53 +0300

    Changed: Check permission and create link or text in users table

--- resources/views/users/users-table.blade.php
+++ resources/views/users/users-table.blade.php
@@ -2,7 +2,11 @@
     @foreach($data as $user)
         <tr class="{{ $user->online == 'online' ? 'fw-bold' : '' }} {{ $user->is_active == 0 ? 'text-muted' : '' }}" data-page="{{ $data->currentPage() }}">
             <td>
-                <a href="users/{{ $user->id }}" class="text-decoration-none {{ $user->is_active == 0 ? 'link-secondary' : '' }}">{{ $user->full_name }}</a>
+                @permission('users_' . $user->status . '_view')
+                    <a href="users/{{ $user->id }}" class="text-decoration-none {{ $user->is_active == 0 ? 'link-secondary' : '' }}">{{ $user->full_name }}</a>
+                @else_permission
+                    {{ $user->full_name }}
+                @endpermission
             </td>
             <td>{{ $user->phone_formatted }}</td>
             <td>{{ $user->roles[0]->name ?? '---' }}</td>
