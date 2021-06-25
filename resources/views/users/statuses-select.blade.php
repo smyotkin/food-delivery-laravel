7995d61a089924602639d070be9ff7d3f1b57e1b
smyotkin <smyotkin@gmail.com>
2021-06-25 09:17:51 +0300

    Изменено: Выбор статуса - вывод только доступных статусов пользователю

--- resources/views/users/statuses-select.blade.php
+++ resources/views/users/statuses-select.blade.php
@@ -2,7 +2,9 @@
 
 <select class="form-select" id="status" name="status" required>
     <option disabled selected>Ничего не выбрано</option>
-    @foreach ($statuses as $key => $status)
-        <option value="{{ $key }}" {{ old('status') == $key || isset($role->status) && $role->status == $key ? 'selected' : '' }}>{{ $status['name'] }}</option>
+    @foreach ($statuses as $slug => $status)
+        @if (in_array($slug, $available_statuses) || (isset($user) && $user->isRoot()))
+            <option value="{{ $slug }}" {{ old('status') == $slug || isset($role->status) && $role->status == $slug ? 'selected' : '' }}>{{ $status['name'] }}</option>
+        @endif
     @endforeach
 </select>
