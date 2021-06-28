9b06a7410e482a306e49601c92f57ed70c7540e2
smyotkin <smyotkin@gmail.com>
2021-06-28 08:41:25 +0300

    Изменено: Подключение Sweet Alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         <script src="{{ asset('js/custom.js') }}"></script>
     </head>
     <body class="font-sans antialiased">
