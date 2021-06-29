3bb59ece0cc3d07f1a2a836cd99786d218e94948
smyotkin <smyotkin@gmail.com>
2021-06-29 11:50:04 +0300

    Изменено: Показ статусов (все статусы если root-пользователь) или по наличию прав

--- resources/views/users/statuses-select.blade.php
+++ resources/views/users/statuses-select.blade.php
@@ -3,7 +3,7 @@
 <select class="form-select" id="status" name="status" required>
     <option disabled selected>Ничего не выбрано</option>
     @foreach ($statuses as $slug => $status)
-        @if (in_array($slug, $available_statuses) || (isset($user) && $user->isRoot()))
+        @if (Auth::user()->isRoot() || in_array($slug, $available_statuses))
             <option value="{{ $slug }}" {{ old('status') == $slug || isset($role->status) && $role->status == $slug ? 'selected' : '' }}>{{ $status['name'] }}</option>
         @endif
     @endforeach
