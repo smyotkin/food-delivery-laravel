db903ca17a96040cd2a03d71ed3b4b557c7f4236
smyotkin <smyotkin@gmail.com>
2021-06-30 09:39:33 +0300

    Изменено: метод index в Profile Controller

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Models\Permission;
 use App\Services\UsersService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,8 +18,24 @@ class ProfileController extends Controller
      */
     public function index(Request $request): string
     {
+        $user = UsersService::getOrFail(['id' => Auth::user()->id]);
+        $role = UsersService::getRoleWithPermissions(['id' => Auth::user()->id]);
+
+        if ($is_custom_permissions = !empty($user->is_custom_permissions)) {
+            $currentPermissions = UsersService::getPermissions(['id' => Auth::user()->id])->pluck('slug')->toArray();
+        } else {
+            $currentPermissions = !empty($role) ? $role->permissions->pluck('slug')->toArray() : [];
+        }
+
+        $filteredPermissions = Permission::orderBy('group', 'desc')->get()->filter(function ($item) use
+        ($currentPermissions) {
+            return in_array($item->slug, $currentPermissions);
+        })->values();
+
         return view('users/profile', [
-            'user' => UsersService::get(['id' => Auth::user()->id]),
+            'user' => $user,
+            'role' => $role,
+            'current_permissions' => $filteredPermissions,
         ])->render();
     }
 }
