d322c8bc5fc08ce97c099fc5b6c47fde9b712e1a
smyotkin <smyotkin@gmail.com>
2021-07-03 07:46:42 +0300

    Добавлено: новые функции в JS валидации поля и проверки валидации

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,26 +1,33 @@
 $(document).ready(function () {
     $('.ru-phone_format').mask("+7 999 999-99-99", {autoclear: false});
-
-    $('body').on('keyup change', '.update_position :input', function() {
-        checkPositionForm();
-    });
 });
-
-function checkPositionForm() {
-    $('#save').addClass('disabled btn-outline-secondary');
-
-    if ($('#name').val().length < 2)
-        return;
-
-    if ($('#slug').val().length < 2)
-        return;
-
-    $('#save').removeClass('disabled btn-outline-secondary').addClass('btn-outline-primary');
-}
 
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
     ));
+
     return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function validateField(condition, selector, addClass = 'is-invalid') {
+    if (condition) {
+        selector.addClass(addClass);
+
+        return false;
+    }
+
+    return true;
+}
+
+function checkValidation(fields) {
+    let available = true;
+
+    $.each(fields, function(key, value) {
+        if (value === false) {
+            available = false;
+        }
+    });
+
+    return available;
 }
