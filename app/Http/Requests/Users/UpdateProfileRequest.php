c3cb36e308658e21c7633cd9b1d1f1252406281e
smyotkin <smyotkin@gmail.com>
2021-07-04 11:19:50 +0300

    Изменено: Request Profile - удален метод обработки перед валидацией

--- app/Http/Requests/Users/UpdateProfileRequest.php
+++ app/Http/Requests/Users/UpdateProfileRequest.php
@@ -3,22 +3,9 @@
 namespace App\Http\Requests\Users;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Hash;
 
 class UpdateProfileRequest extends FormRequest
 {
-    /**
-     * Обработка перед валидацией
-     *
-     * @return void
-     */
-    protected function prepareForValidation(): void
-    {
-        $this->merge([
-            'password' => Hash::make($this->password),
-        ]);
-    }
-
     /**
      * Determine if the user is authorized to make this request.
      *
