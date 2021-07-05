a782d2fbf1753f64c4c2a398a1ac17820cc200b3
smyotkin <smyotkin@gmail.com>
2021-07-05 10:33:04 +0300

    Добавлено: Сидер Root-пользователя с id = 1, password = 123456, phone = 79112223344 и всеми правами

---
+++ database/seeders/RootUserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class RootUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function run()
+    {
+        $allPermissions = Permission::all();
+
+        $user = User::create([
+            'id' => 1,
+            'city_id' => 0,
+            'first_name' => 'Главный',
+            'last_name' => 'Админ',
+            'password' => Hash::make(123456),
+            'phone' => '79112223344',
+            'timezone' => 'Europe/Moscow',
+            'is_active' => 1,
+            'is_custom_permissions' => 1,
+        ]);
+
+        $user->permissions()->attach($allPermissions);
+    }
+}
