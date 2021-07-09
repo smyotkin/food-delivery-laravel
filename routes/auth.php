35cadd5d603dfb01f4ff6a99dccd9c0f1cd1c928
smyotkin <smyotkin@gmail.com>
2021-07-09 13:19:25 +0300

    Изменено: роутинг Auth - удалены лишние пути и добавлены новые для восстановления пароля через смс-код

--- routes/auth.php
+++ routes/auth.php
@@ -1,13 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
@@ -21,28 +16,21 @@ Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->middleware('guest')
                 ->name('password.request');
 
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+Route::get('/forgot-password/{phone}', [PasswordResetLinkController::class, 'createForm'])
                 ->middleware('guest')
-                ->name('password.email');
+                ->name('password.phone');
 
-Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+Route::post('/forgot-password/{phone}', [PasswordResetLinkController::class, 'sendSmsAjax'])
                 ->middleware('guest')
-                ->name('password.reset');
+                ->name('password.pin');
+
+Route::put('/forgot-password/{phone}', [PasswordResetLinkController::class, 'store'])
+                ->middleware('guest')
+                ->name('password.store');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->middleware('guest')
                 ->name('password.update');
-
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
-                ->name('verification.send');
-
-Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->middleware('auth')
-                ->name('password.confirm');
-
-Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
-                ->middleware('auth');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
