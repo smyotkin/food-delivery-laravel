cfa8c68e927eda57c99edce288843a543179578d
smyotkin <smyotkin@gmail.com>
2021-07-09 13:13:29 +0300

    Добавлено: Миграция Passwords_resets - поле e-mail меняем на phone

---
+++ database/migrations/2021_07_06_211538_change_password_resets_email_to_phone.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
+
+class ChangePasswordResetsEmailToPhone extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('password_resets', function (Blueprint $table) {
+            if (!Type::hasType('char')) {
+                Type::addType('char', StringType::class);
+            }
+
+            $table->char('email', 11)->change();
+            $table->renameColumn('email', 'phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('password_resets', function (Blueprint $table) {
+            //
+        });
+    }
+}
