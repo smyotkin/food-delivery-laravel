4f780c4b90970fda2d864ceebda65b7044106cc5
smyotkin <smyotkin@gmail.com>
2021-07-11 04:45:45 +0300

    Изменено: Custom конфиг - добавлены новые параметры attempts_per_day, pin_lifetime_minutes и send_sms

--- config/custom.php
+++ config/custom.php
@@ -6,4 +6,9 @@ return [
         'ferone' => 'back.ferone-laravel.ru',
         'site' => 'suare-laravel.su',
     ],
+    'password_resets' => [
+        'attempts_per_day' => 10,
+        'pin_lifetime_minutes' => 5,
+        'send_sms' => 0,
+    ]
 ];
