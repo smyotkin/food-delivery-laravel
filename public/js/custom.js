87172bfdab709ab8e2937da852af9f82b7ddcfdb
smyotkin <smyotkin@gmail.com>
2021-07-11 18:50:36 +0300

    Изменено: custom.js - новая функция setCursorToActive (устанавливает курсор в активную позицию)

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
     $('.ru-phone_format').mask("+7 999 999-99-99", {autoclear: false});
+
+    setCursorToActive('.ru-phone_format');
 });
+
+function setCursorToActive(element) {
+    let pos = 0;
+
+    $('body').on('change', element, function(event) {
+        pos = $.inArray('_', $(this).val());
+    });
+
+    $('body').on('click', element, function(event) {
+        setCaretToPos($(this), pos);
+        pos = $.inArray('_', $(this).val());
+    });
+
+    function setCaretToPos(input, pos) {
+        input[0].setSelectionRange(pos, pos);
+    }
+}
 
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
