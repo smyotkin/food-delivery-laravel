2f15c9d109771ad85f00ef1964d964b3f0bf5883
smyotkin <smyotkin@gmail.com>
2021-07-15 10:04:07 +0300

    Изменено: Users Statuses Select шаблон - добавлено новое условие при наличии должности отображать статус должности

--- resources/views/users/statuses-select.blade.php
+++ resources/views/users/statuses-select.blade.php
@@ -3,8 +3,8 @@
 <select class="form-select" id="status" name="status" required>
     <option disabled selected>Ничего не выбрано</option>
     @foreach ($statuses as $slug => $status)
-        @if (Auth::user()->isRoot() || in_array($slug, $available_statuses))
-            <option value="{{ $slug }}" {{ old('status') == $slug || isset($role->status) && $role->status == $slug ? 'selected' : '' }}>{{ $status['name'] }}</option>
+        @if (Auth::user()->isRoot() || in_array($slug, $available_statuses) || (isset($role->status) && $slug == $role->status))
+            <option value="{{ $slug }}" {{ old('status') == $slug || (isset($role->status) && $role->status == $slug) ? 'selected' : '' }}>{{ $status['name'] }}</option>
         @endif
     @endforeach
 </select>
