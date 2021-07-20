11f5e0a7d7411f78dee6df87ebc9c01561cc5ed4
smyotkin <smyotkin@gmail.com>
2021-07-20 08:47:37 +0300

    Изменено: Settings Controller - удалены неиспользуемые методы, обновление настройки пересено из store в update, в методе getAjax добавлены все настройки

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -29,28 +29,19 @@ class SettingsController extends Controller
         $allSettings = SettingsService::find($request->toArray());
 
         return view('settings/settings-table', [
+            'settings' => $allSettings,
             'formatted' => SettingsService::formatSetting($allSettings->toArray()['data']),
         ])->render();
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return void
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Update the specified resource in storage.
      *
      * @param \Illuminate\Http\Request $request
      * @return false|string
      * @throws \Throwable
      */
-    public function store(Request $request)
+    public function update(Request $request)
     {
         SettingsService::update($request->all());
 
@@ -59,50 +50,6 @@ class SettingsController extends Controller
         ], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_FORCE_OBJECT|JSON_UNESCAPED_UNICODE);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 
     public function clearCache()
     {
