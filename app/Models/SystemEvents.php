5248c63d21c6af953eb4a2dbc8177c4b032a0666
smyotkin <smyotkin@gmail.com>
2021-07-24 06:49:28 +0300

    Изменено: System Events Model - добавлены заполняемые столбцы, мутатор возвращаемый форматированную дату, и отключено использование updated_at

--- app/Models/SystemEvents.php
+++ app/Models/SystemEvents.php
@@ -12,18 +12,34 @@ class SystemEvents extends Model
 
     protected $appends = [
         'date',
-        'user'
+        'user',
     ];
+
+    protected $fillable = [
+        'slug',
+        'label',
+        'msg',
+        'data',
+        'user_id',
+        'created_at',
+    ];
+
+    public const UPDATED_AT = null;
 
     public function getDateAttribute()
     {
         $date = Date::parse($this->created_at);
 
-        return $date->format(now()->year == $date->year ? 'j F, H:i' : 'j F Y');
+        return $date->format(now()->year == $date->year ? 'j F, H:i' : 'j F Y, H:i');
     }
 
     public function getUserAttribute()
     {
         return User::find($this->user_id) ?? false;
+    }
+
+    public function getDataFormattedAttribute()
+    {
+        return json_encode(json_decode($this->data, true), JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
     }
 }
