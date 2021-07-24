8ae7c8ca271979b13880d816cb85397a57f9db88
smyotkin <smyotkin@gmail.com>
2021-07-24 06:54:00 +0300

    Изменено: System Events Controller - удалены лишние методы, добавлен экспорт в csv

--- app/Http/Controllers/SystemEventsController.php
+++ app/Http/Controllers/SystemEventsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\SystemEventsExport;
 use App\Services\SystemService;
 use Illuminate\Http\Request;
 
@@ -18,56 +19,12 @@ class SystemEventsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
     {
         //
     }
@@ -87,17 +44,23 @@ class SystemEventsController extends Controller
      * Экспорт в CSV
      *
      * @param Request $request
-//     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\BinaryFileResponse
      */
     public function exportEventsCsv(Request $request)
     {
-//        return (new UsersExport(UsersService::find($request->toArray())))->download();
+        return (new SystemEventsExport(SystemService::findEvents($request->toArray())))->download();
     }
 
-    public function getEventsAjax (Request $request)
+    /**
+     * Получить список событий через Ajax
+     *
+     * @param Request $request
+     * @return string
+     */
+    public function getEventsAjax(Request $request)
     {
         return view('system/events-table', [
-            'data' => SystemService::find($request->toArray()),
+            'data' => SystemService::findEvents($request->toArray()),
         ])->render();
     }
 }
