e48f9576c4a8f246b6d3a99705ed558e02bba717
smyotkin <smyotkin@gmail.com>
2021-07-24 06:59:32 +0300

    Изменено: Events Table - добавлено модальное окно для вывода дополнительных данных события

--- resources/views/system/events-table.blade.php
+++ resources/views/system/events-table.blade.php
@@ -2,13 +2,31 @@
     @foreach($data as $event)
         <tr>
             <td>{{ $event['date'] }}</td>
-            <td>{{ $event['name'] }}</td>
+            <td>{{ $event['label'] }}</td>
             <td>{{ $event['user']->full_name }}</td>
             <td>
                 <div class="row">
                     <div class="col">
                         {{ $event['msg'] }}
-                        <button type="button" class="btn btn-info btn-sm rounded-circle ms-1 px-2 py-0">i</button>
+
+                        @if (!empty($event['data']))
+                            <button type="button" class="btn btn-primary btn-sm rounded-circle ms-1 px-2 py-0" data-bs-toggle="modal" data-bs-target="#event_data_{{ $event['id'] }}">i</button>
+
+                            <div class="modal fade" id="event_data_{{ $event['id'] }}" tabindex="-1" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel"><strong>Дополнительные данные:</strong></h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+
+                                        <div class="modal-body">
+                                            <pre>{{ $event['data_formatted'] }}</pre>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </td>
