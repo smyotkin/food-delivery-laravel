0a9c9f663463ef242ff9eefd4b8c5351f19f19b1
smyotkin <smyotkin@gmail.com>
2021-07-26 13:21:07 +0300

    Изменено: Ferone Route - новый маршрут 'system/notifications' и изменены пути к переименованным классам

--- routes/ferone.php
+++ routes/ferone.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\SettingsController;
-use App\Http\Controllers\SystemEventsController;
-use App\Http\Controllers\SystemLogController;
+use App\Http\Controllers\System\NotificationsController;
+use App\Http\Controllers\System\EventsController;
+use App\Http\Controllers\System\LogController;
 use Illuminate\Support\Facades\Route;
 use Rap2hpoutre\LaravelLogViewer\LogViewerController;
 
@@ -28,23 +29,26 @@ Route::middleware(['user.is_active'])->group(function () {
             'index', 'update'
         ]);
 
-        Route::get('system/events', [SystemEventsController::class, 'index'])
+        Route::get('system/events', [EventsController::class, 'index'])
             ->name('events.index');
-        Route::get('system/events/export.csv', [SystemEventsController::class, 'exportEventsCsv']);
-        Route::post('system/events/clear', [SystemEventsController::class, 'clearEvents'])
+        Route::get('system/events/export.csv', [EventsController::class, 'exportEventsCsv']);
+        Route::post('system/events/clear', [EventsController::class, 'clearEvents'])
             ->name('system/events/clear');
-        Route::get('system/events/get.ajax', [SystemEventsController::class, 'getEventsAjax'])
+        Route::get('system/events/get.ajax', [EventsController::class, 'getEventsAjax'])
             ->name('system/events/get.ajax');
 
-        Route::get('system/log', [SystemLogController::class, 'index'])
+        Route::get('system/log', [LogController::class, 'index'])
             ->name('log.index');
-        Route::post('system/log/clear', [SystemLogController::class, 'clear'])
+        Route::post('system/log/clear', [LogController::class, 'clear'])
             ->name('system/log/clear');
-        Route::get('system/log/get.ajax', [SystemLogController::class, 'getAjax'])
+        Route::get('system/log/get.ajax', [LogController::class, 'getAjax'])
             ->name('system/log/get.ajax');
 
         Route::get('system/logs', [LogViewerController::class, 'index'])
             ->name('logs.index');
+
+        Route::get('system/notifications', [NotificationsController::class, 'index'])
+            ->name('notifications.index');
     });
 });
 
