548887775da7dcbd17503977e4233f2366cd8d91
smyotkin <smyotkin@gmail.com>
2021-07-28 13:22:26 +0300

    Изменено: Services Config - добавлен параметр 'telegram-bot-api'

--- config/services.php
+++ config/services.php
@@ -35,4 +35,8 @@ return [
         'secret' => env('SMSCRU_SECRET'),
         'sender' => env('SMSCRU_SENDER'),
     ],
+
+    'telegram-bot-api' => [
+        'token' => env('TELEGRAM_BOT_TOKEN', '1869112598:AAGZ5kSGt52535tEhPpwvibXhVlQicXe8ms'),
+    ],
 ];
