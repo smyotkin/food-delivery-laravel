5f46bac861280469db87153c6ae89e1aa5a55e16
smyotkin <smyotkin@gmail.com>
2021-07-30 09:14:17 +0300

    Добавлено: миграция Modules Table

---
+++ database/migrations/2021_07_29_103542_create_modules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('url');
+            $table->tinyInteger('is_control')->unsigned();
+            $table->tinyInteger('is_active')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modules');
+    }
+}
