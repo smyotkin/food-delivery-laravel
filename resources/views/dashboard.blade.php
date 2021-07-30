652891e6c2e7659589b9e2c1162a2fa77b0f4684
smyotkin <smyotkin@gmail.com>
2021-07-30 09:38:09 +0300

    Изменено: Dashboard Blade - изменен шаблон под циклы и проверку прав на каждый модуль

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,100 +5,41 @@
     <div class="container-fluid bg-gradient-ferone min-vh-100 d-flex align-items-center" style="padding-top: 55px">
         <div class="container">
             <div class="row">
-                <div class="col-8 py-3 rounded rounded-5" style="background-color: rgba(0,0,0,0.3)">
-                    <div class="row">
-                        <div class="col-3">
-                            <a href="javascript:" class="text-decoration-none text-center text-white fs-5">
-                                <div class="text-center">
-                                    <img src="/img/callcenter-icon.png" class="rounded rounded-5 shadow">
-                                </div>
-                                <div class="name mt-3 lh-1 text-shadow">Колл-центр</div>
-                            </a>
-                        </div>
-                        <div class="col-3">
-                            <a href="javascript:" class="text-decoration-none text-center text-white fs-5">
-                                <div class="text-center">
-                                    <img src="/img/kitchen-icon.png" class="rounded rounded-5 shadow">
-                                </div>
-                                <div class="name mt-3 lh-1 text-shadow">Кухня</div>
-                            </a>
-                        </div>
-                        <div class="col-3">
-                            <a href="javascript:" class="text-decoration-none text-center text-white fs-5">
-                                <div class="text-center">
-                                    <img src="/img/packing-icon.png" class="rounded rounded-5 shadow">
-                                </div>
-                                <div class="name mt-3 lh-1 text-shadow">Сборка</div>
-                            </a>
-                        </div>
-                        <div class="col-3">
-                            <a href="javascript:" class="text-decoration-none text-center text-white fs-5">
-                                <div class="text-center">
-                                    <img src="/img/delivery-icon.png" class="rounded rounded-5 shadow">
-                                </div>
-                                <div class="name mt-3 lh-1 text-shadow">Доставка</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 py-3">
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="javascript:" class="text-decoration-none text-center text-white fs-5">
-                                <div class="text-center">
-                                    <img src="/img/clients-icon.png" class="rounded rounded-5 shadow">
-                                </div>
-                                <div class="name mt-3 lh-1 text-shadow">Клиенты</div>
-                            </a>
-                        </div>
+                @php ($controlCount = $control->count())
 
-                        @permission('users_view')
-                            <div class="col-6">
-                                <a href="/users" class="text-decoration-none text-center text-white fs-5">
-                                    <div class="text-center">
-                                        <img src="/img/users-icon.png" class="rounded rounded-5 shadow">
-                                    </div>
-                                    <div class="name mt-3 lh-1 text-shadow">Пользователи</div>
-                                </a>
+                @if ($controlCount > 0)
+                    <div class="col-{{ $controlCount * 2 }} d-flex align-items-center rounded rounded-5" style="background-color: rgba(0, 0, 0, 0.3)">
+                        <div class="w-100">
+                            <div class="row">
+                                @foreach($control as $module)
+                                    @permission($module->slug . '_view')
+                                        <div class="col-{{ 12 / $controlCount }}">
+                                            <a href="{{ $module->url }}" class="text-decoration-none text-center text-white fs-5">
+                                                <div class="text-center">
+                                                    <img src="/img/{{ $module->slug }}-icon.png" class="rounded rounded-5 shadow">
+                                                </div>
+                                                <div class="name mt-3 lh-1 text-shadow">{{ $module->name }}</div>
+                                            </a>
+                                        </div>
+                                    @endpermission
+                                @endforeach
                             </div>
-                        @endpermission
+                        </div>
                     </div>
-                </div>
-            </div>
+                @endif
 
-            <div class="row mt-5">
-                <div class="col-2">
-                    <a href="javascript:" class="text-decoration-none text-center text-white fs-5">
-                        <div class="text-center">
-                            <img src="/img/menu-icon.png" class="rounded rounded-5 shadow">
+                @foreach($other as $module)
+                    @permission($module->slug . '_view')
+                        <div class="col-2 d-flex align-items-center justify-content-center py-4">
+                            <a href="{{ $module->url }}" class="text-decoration-none text-center text-white fs-5">
+                                <div class="text-center">
+                                    <img src="/img/{{ $module->slug }}-icon.png" class="rounded rounded-5 shadow">
+                                </div>
+                                <div class="name mt-3 lh-1 text-shadow">{{ $module->name }}</div>
+                            </a>
                         </div>
-                        <div class="name mt-3 lh-1 text-shadow">Меню</div>
-                    </a>
-                </div>
-                <div class="col-2">
-                    <a href="javascript:" class="text-decoration-none text-center text-white fs-5">
-                        <div class="text-center">
-                            <img src="/img/stores-icon.png" class="rounded rounded-5 shadow">
-                        </div>
-                        <div class="name mt-3 lh-1 text-shadow">Заведения</div>
-                    </a>
-                </div>
-                <div class="col-2">
-                    <a href="javascript:" class="text-decoration-none text-center text-white fs-5">
-                        <div class="text-center">
-                            <img src="/img/dictionaries-icon.png" class="rounded rounded-5 shadow">
-                        </div>
-                        <div class="name mt-3 lh-1 text-shadow">Справочники</div>
-                    </a>
-                </div>
-                <div class="col-2">
-                    <a href="/settings" class="text-decoration-none text-center text-white fs-5">
-                        <div class="text-center">
-                            <img src="/img/settings-icon.png" class="rounded rounded-5 shadow">
-                        </div>
-                        <div class="name mt-3 lh-1 text-shadow">Настройки</div>
-                    </a>
-                </div>
+                    @endpermission
+                @endforeach
             </div>
         </div>
     </div>
