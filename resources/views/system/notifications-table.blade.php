68c7cdeac4b28ebd8ef2b991c4c72cf599555c35
smyotkin <smyotkin@gmail.com>
2021-08-02 10:13:45 +0300

    Изменено: Notifications Table Blade - изменен вывод шаблона сообщения, добавлен nl2br

--- resources/views/system/notifications-table.blade.php
+++ resources/views/system/notifications-table.blade.php
@@ -28,7 +28,7 @@
 
                             <div class="row">
                                 <div class="col d-flex align-items-center">
-                                    <a href="javascript:" onclick="$(this).hide()" class="notification_option btn text-start text-decoration-none text-dark rounded-3 border border-dark w-100 px-3 py-2">{{ $notification['msg_template'] }}</a>
+                                    <a href="javascript:" onclick="$(this).hide()" class="notification_option btn text-start text-decoration-none text-dark rounded-3 border border-dark w-100 px-3 py-2">{!! nl2br($notification['msg_template']) !!}</a>
 
                                     <textarea class="d-none form-control" name="msg_template" rows="1">{{ $notification['msg_template'] }}</textarea>
                                 </div>
