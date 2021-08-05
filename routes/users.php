b5f4ad4a74886aa305f4b499e7079cc06ad1c83a
smyotkin <smyotkin@gmail.com>
2021-08-05 15:41:27 +0300

    Изменено: Users Route - все маршруты пользоватлей разбиты на отдельные части и с проверкой 1 маршрут = 1 право (либо любое из указанных), убрана иерархия проверок прав доступа

--- routes/users.php
+++ routes/users.php
@@ -10,20 +10,33 @@ use App\Http\Controllers\Profile\ProfileController;
  */
 
 Route::middleware(['auth'])->group(function () {
+
+    /**
+     * AJAX - Users
+     */
     Route::get('/users/getAjax', [UsersController::class, 'getAjax'])
         ->name('users/getAjax');
 
     Route::get('/users/getUserFormAjax', [UsersController::class, 'getUserFormAjax'])
         ->name('users.getUserFormAjax');
 
+    Route::get('/profile/getAjax', [ProfileController::class, 'getAjax'])
+        ->name('profile.getAjax');
+
+    Route::get('/users/getPermissionsCheckedAjax', [UsersController::class, 'getPermissionsCheckedAjax'])
+        ->name('users.getPermissionsCheckedAjax');
+
+    Route::get('/users/export.csv', [UsersController::class, 'exportCsv']);
+
+    /**
+     * AJAX - Positions
+     */
     Route::get('/users/positions/getFormAjax', [PositionsController::class, 'getFormAjax'])
         ->name('positions.getFormAjax');
 
     Route::get('/users/positions/getAjax', [PositionsController::class, 'getAjax'])
+        ->middleware(['permissions:users_positions_view'])
         ->name('positions.getAjax');
-
-    Route::get('/profile/getAjax', [ProfileController::class, 'getAjax'])
-        ->name('profile.getAjax');
 
     Route::get('/users/positions/getAjaxByStatus', [PositionsController::class, 'getAjaxByStatus'])
         ->name('positions.getAjaxByStatus');
@@ -31,28 +44,73 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/users/positions/getWithPermissions', [PositionsController::class, 'getWithPermissions'])
         ->name('positions.getWithPermissions');
 
-    Route::get('/users/getPermissionsCheckedAjax', [UsersController::class, 'getPermissionsCheckedAjax'])
-        ->name('users.getPermissionsCheckedAjax');
-
-    Route::get('/users/export.csv', [UsersController::class, 'exportCsv']);
-
+    /**
+     * Http - Positions
+     */
     Route::middleware(['last.page'])->group(function () {
-        Route::middleware(['permissions:users_positions_view'])->group(function () {
-            Route::resource('users/positions', PositionsController::class)->except([
-                'edit'
-            ]);
+        Route::middleware(['permissions:users_position_create'])->group(function () {
+            Route::get('users/positions/create', [PositionsController::class, 'create'])
+                ->name('positions.create');
+
+            Route::post('users/positions', [PositionsController::class, 'store'])
+                ->name('positions.store');
         });
 
-        Route::middleware(['permissions:users_view'])->group(function () {
-            Route::resource('users', UsersController::class)->except([
-                'edit'
-            ]);
+        Route::get('users/positions', [PositionsController::class, 'index'])
+            ->middleware(['permissions:users_positions_view'])
+            ->name('positions.index');
+
+        Route::get('users/positions/{role}', [PositionsController::class, 'show'])
+            ->middleware(['any-permissions:users_position_view||users_position_modify'])
+            ->name('positions.show');
+
+        Route::match(['put', 'patch'], 'users/positions/{position}', [PositionsController::class, 'update'])
+            ->middleware(['permissions:users_position_modify'])
+            ->name('positions.update');
+
+        Route::delete('users/positions/{position}', [PositionsController::class, 'destroy'])
+            ->middleware(['permissions:users_position_delete'])
+            ->name('positions.destroy');
+    });
+
+    /**
+     * Http - Users
+     */
+    Route::middleware(['last.page'])->group(function () {
+        Route::middleware(['any-permissions:users_employee_add||users_specialist_add||users_head_add||users_owner_add'])->group(function () {
+            Route::get('users/create', [UsersController::class, 'create'])
+                ->name('users.create');
+
+            Route::post('users', [UsersController::class, 'store'])
+                ->name('users.store');
         });
 
+        Route::get('users', [UsersController::class, 'index'])
+            ->middleware(['permissions:users_view'])
+            ->name('users.index');
+
+        Route::get('users/{user}', [UsersController::class, 'show'])
+            ->middleware(['any-permissions:users_employee_view||users_specialist_view||users_head_view||users_owner_view||users_employee_modify||users_specialist_modify||users_head_modify||users_owner_modify'])
+            ->name('users.show');
+
+        Route::match(['put', 'patch'], 'users/{user}', [UsersController::class, 'update'])
+            ->middleware(['any-permissions:users_employee_modify||users_specialist_modify||users_head_modify||users_owner_modify'])
+            ->name('users.update');
+
+        Route::delete('users/{user}', [UsersController::class, 'destroy'])
+            ->middleware(['any-permissions:users_employee_delete||users_specialist_delete||users_head_delete||users_owner_delete'])
+            ->name('users.destroy');
+    });
+
+    /**
+     * Http - User Profile
+     */
+    Route::middleware(['last.page'])->group(function () {
         Route::resource('profile', ProfileController::class)->only([
             'index', 'update'
         ]);
     });
+
 });
 
 
