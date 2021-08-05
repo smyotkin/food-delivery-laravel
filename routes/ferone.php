d17c4917573ad6efe780566b50f7691bd69a8a24
smyotkin <smyotkin@gmail.com>
2021-08-05 15:41:54 +0300

    Изменено: Users Route - все маршруты ферона разбиты на отдельные части и с проверкой 1 маршрут = 1 право (либо любое из указанных), убрана иерархия проверок прав доступа

--- routes/ferone.php
+++ routes/ferone.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\System\NotificationsController;
 use App\Http\Controllers\System\EventsController;
-use App\Http\Controllers\System\LogController;
 use Illuminate\Support\Facades\Route;
 use Rap2hpoutre\LaravelLogViewer\LogViewerController;
 
@@ -17,39 +16,85 @@ Route::get('/', function () {
 Route::middleware(['user.is_active'])->group(function () {
     require __DIR__.'/users.php';
 
-    Route::middleware(['auth', 'last.page'])->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'index'])
-            ->name('dashboard');
+    Route::middleware(['auth'])->group(function () {
 
-        Route::get('/settings/get.ajax', [SettingsController::class, 'getAjax'])
+        /**
+         * AJAX - Settings
+         */
+        Route::get('settings/get.ajax', [SettingsController::class, 'getAjax'])
+            ->middleware(['permissions:settings_view'])
             ->name('settings/get.ajax');
-        Route::post('settings/clear.cache', [SettingsController::class, 'clearCache'])
-            ->name('settings/clear.cache');
-        Route::resource('settings', SettingsController::class)->only([
-            'index', 'update'
-        ]);
 
+        Route::post('settings/clear.cache', [SettingsController::class, 'clearCache'])
+            ->middleware(['permissions:settings_modify'])
+            ->name('settings/clear.cache');
+
+        /**
+         * AJAX - Settings/Events
+         */
         Route::middleware(['permissions:events_modify_and_view'])->group(function () {
-            Route::get('system/events', [EventsController::class, 'index'])
-                ->name('events.index');
             Route::get('system/events/export.csv', [EventsController::class, 'exportEventsCsv']);
+
             Route::post('system/events/clear', [EventsController::class, 'clearEvents'])
                 ->name('system/events/clear');
+
             Route::get('system/events/get.ajax', [EventsController::class, 'getEventsAjax'])
                 ->name('system/events/get.ajax');
         });
 
-        Route::middleware(['permissions:log_modify_and_view'])->group(function () {
-            Route::get('system/logs', [LogViewerController::class, 'index'])
-                ->name('logs.index');
-        });
+        /**
+         * AJAX - Settings/Notifications
+         */
+        Route::get('system/notifications/get.ajax', [NotificationsController::class, 'getAjax'])
+            ->middleware(['permissions:notifications_modify_and_view'])
+            ->name('system/notifications/get.ajax');
 
-        Route::middleware(['permissions:notifications_modify_and_view'])->group(function () {
-            Route::get('system/notifications/get.ajax', [NotificationsController::class, 'getAjax'])
-                ->name('system/notifications/get.ajax');
-            Route::resource('system/notifications', NotificationsController::class)->only([
-                'index', 'update'
-            ]);
+
+        /**
+         * Middleware - Last Page
+         */
+        Route::middleware(['last.page'])->group(function () {
+
+            /**
+             * Http - Dashboard
+             */
+            Route::get('dashboard', [DashboardController::class, 'index'])
+                ->name('dashboard');
+
+            /**
+             * Http - Settings
+             */
+            Route::get('settings', [SettingsController::class, 'index'])
+                ->middleware(['permissions:settings_view'])
+                ->name('settings.index');
+
+            Route::match(['put', 'patch'], 'settings/{setting}', [SettingsController::class, 'update'])
+                ->middleware(['permissions:settings_modify'])
+                ->name('settings.update');
+
+            /**
+             * Http - Settings/Events
+             */
+            Route::get('system/events', [EventsController::class, 'index'])
+                ->middleware(['permissions:events_modify_and_view'])
+                ->name('events.index');
+
+            /**
+             * Http - Settings/Logs
+             */
+            Route::get('system/logs', [LogViewerController::class, 'index'])
+                ->middleware(['permissions:log_modify_and_view'])
+                ->name('logs.index');
+
+            /**
+             * Http - Settings/Notifications
+             */
+            Route::resource('system/notifications', NotificationsController::class)
+                ->middleware(['permissions:notifications_modify_and_view'])
+                ->only([
+                    'index',
+                    'update',
+                ]);
         });
     });
 });
