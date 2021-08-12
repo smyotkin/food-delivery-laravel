ab8324e7502a1dd6ec7d15fa0406cc19a413a625
smyotkin <smyotkin@gmail.com>
2021-08-12 08:36:07 +0300

    Изменено: Site Routes - добавлен маршрут '/telegram-webhook' и код вебхука для обработки команды start и отправки chat id обратно пользователю

--- routes/site.php
+++ routes/site.php
@@ -1,8 +1,36 @@
 <?php
 
+use App\Notifications\Telegram;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (Request $request) {
     return view('welcome');
+});
+
+Route::post('/telegram-webhook', function () {
+    $command = 'start';
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+
+    if (empty($data) || !isset($data['message']['text'])) {
+        return;
+    }
+
+    if (@$data['message']['from']['is_bot']) {
+        return;
+    }
+
+    if (!empty($command) && !preg_match("/^\/{$command}/i", $data['message']['text']) && $data['message']['entities']['type'] == 'bot_command') {
+        return;
+    }
+
+    $chatId = $data['message']['chat']['id'] ?? false;
+
+    if ($chatId) {
+        Notification::route('telegram', $chatId)
+            ->notify(new Telegram([
+                'msg' => 'Ваш ID чата: ' . $chatId,
+            ]));
+    }
 });
