db316af1ea81caa2bd9e5ce0afd984c37779975e
smyotkin <smyotkin@gmail.com>
2021-08-26 10:26:02 +0300

    Изменено: Table Next Blade - в шаблоне "Показать еще" убран текст "пользователей" (т.к. шаблон универсальный, сейчас выглядит "Показать еще 2") и число теперь берет значение настройки global_rows_per_page

--- resources/views/vendor/pagination/table-next.blade.php
+++ resources/views/vendor/pagination/table-next.blade.php
@@ -1,8 +1,11 @@
 @if ($paginator->hasPages() && $paginator->hasMorePages())
+    @php($countPerPage = $paginator->perPage())
+
     <tr class="table_pagination">
-        <td class="bg-white border-0 py-3" colspan="6">
+        <td class="bg-white border-0 py-3" colspan="7">
             <a class="btn btn-link text-decoration-none px-0" href="{{ $paginator->nextPageUrl() }}" rel="next">
-                Показать еще 100 пользователей
+                Показать еще {{ $countPerPage }}
+{{--                {{ Lang::choice('строку|строки|строк', $countPerPage) }}--}}
             </a>
         </td>
     </tr>
