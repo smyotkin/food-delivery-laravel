2e32e60e44ded5483400302131948dde922c410b
smyotkin <smyotkin@gmail.com>
2021-08-26 10:17:59 +0300

    Изменено: Helpers - добавлена функция mb_ucfirst (для работы с русской кодировкой)

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,17 @@
 <?php
 
-if (!function_exists('has_permission')) {
-    function has_permission(string $permission) {
+if (!function_exists('has_permission'))
+{
+    function has_permission(string $permission)
+    {
         return Auth::check() && (Auth::user()->hasPermission($permission) || Auth::user()->isRoot());
+    }
+}
+
+if (!function_exists('mb_ucfirst'))
+{
+    function mb_ucfirst($text)
+    {
+        return mb_strtoupper(mb_substr($text, 0, 1)) . mb_substr($text, 1);
     }
 }
