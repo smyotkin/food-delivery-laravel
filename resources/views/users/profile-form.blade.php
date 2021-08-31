d3fd9ba0a311d867650f7ce95e00eeb6fff01f3b
smyotkin <smyotkin@gmail.com>
2021-08-31 19:36:58 +0300

    Изменено: Profile Form Blade - добавлена адаптивность (под мобильные устройства)

--- resources/views/users/profile-form.blade.php
+++ resources/views/users/profile-form.blade.php
@@ -1,10 +1,10 @@
 <div class="border-bottom border-gray pt-2 pb-4">
-    <div class="row px-5">
-        <div class="col-3">
+    <div class="row px-md-5">
+        <div class="col-12 col-md-3 text-center text-md-start">
             <h4 class="fw-normal mb-0">Права доступа</h4>
         </div>
 
-        <div class="col-7">
+        <div class="col-12 col-md-7">
             @php ($previousGroupValue = '')
             @php ($nextGroupValue = '')
 
@@ -13,7 +13,7 @@
 
                 @if ($permission->group != $previousGroupValue)
                     <div class="row mb-2">
-                        <div class="col-5">
+                        <div class="col-12 col-md-5 my-4 my-md-0">
                             @php ($iconPath = "img/{$permission->group_slug}-icon.png")
 
                             <div class="row">
@@ -26,11 +26,11 @@
                             </div>
                         </div>
 
-                        <div class="col-7">
+                        <div class="col-12 col-md-7">
                             <ul class="list-group list-group-flush">
                                 @endif
 
-                                <li class="list-group-item">{{ $permission->name }}</li>
+                                <li class="list-group-item px-0 px-md-auto">{{ $permission->name }}</li>
 
                                 @if ($permission->group != $nextGroupValue || $loop->last)
                             </ul>
@@ -51,22 +51,22 @@
 </div>
 
 <div class="border-bottom border-gray py-4">
-    <div class="row px-5">
-        <div class="col-3">
+    <div class="row px-0 px-md-5">
+        <div class="col-12 col-md-3">
             <h4 class="fw-normal mb-0">Доступ к данным</h4>
         </div>
-        <div class="col-6">
+        <div class="col-12 col-md-6 mt-2 mt-md-0">
             <p class="mb-0 text-muted">Пусто</p>
         </div>
     </div>
 </div>
 
 <div class="border-bottom border-gray py-4">
-    <div class="row px-5">
-        <div class="col-3">
+    <div class="row px-md-5">
+        <div class="col-12 col-md-3">
             <h4 class="fw-normal mb-0">Пароль</h4>
         </div>
-        <div class="col-4">
+        <div class="col-12 col-md-4 mt-2 mt-md-0">
             <button class="btn btn-link px-0 text-decoration-none" id="change_password">Изменить пароль...</button>
 
             <form action="{{ route('profile.update', ['profile' => $user]) }}" method="post" class="change_password-form needs-validation d-none" novalidate>
@@ -74,7 +74,7 @@
                 @csrf
 
                 <fieldset class="row">
-                    <div class="col">
+                    <div class="col-12 col-md mt-2 mt-md-0">
                         <input class="form-control" type="password" name="password" id="password" value="" placeholder="Новый пароль">
                         <div class="invalid-feedback text-sm">
                             Поле обязательное, не менее 6 симв.
@@ -83,7 +83,7 @@
                         <p class="form-text-info text-sm mt-3 text-muted">Введите новый пароль, минимум шесть символов. Пароль будет выслан на указанный номер телефона</p>
                     </div>
 
-                    <div class="col-auto">
+                    <div class="col-12 col-md-auto text-center text-md-start">
                         <button class="btn btn-link text-decoration-none px-0 disabled" id="change_password-submit" type="submit">Сохранить</button>
                     </div>
                 </fieldset>
@@ -93,8 +93,8 @@
 </div>
 
 <div class="py-4">
-    <div class="row px-5">
-        <div class="col-3">
+    <div class="row px-md-5">
+        <div class="col-12 col-md-3">
             <form method="post" action="{{ route('logout') }}">
                 @csrf
 
@@ -103,7 +103,8 @@
                 </a>
             </form>
         </div>
-        <div class="col-4">
+
+        <div class="col-12 col-md-4 mt-2 mt-md-0">
             @php ($updated_at = Date::parse($user->updated_at))
 
             Последние изменение: {{ $user->first_name }} {{ $user->last_name }}, {{ $updated_at->format( now()->year == $updated_at->year ? 'j F, H:i' : 'j F Y') }}
