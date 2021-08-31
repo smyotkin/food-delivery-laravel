666d9019ab4c4e7972ab2fc8b859035b114e89d1
smyotkin <smyotkin@gmail.com>
2021-08-31 19:34:28 +0300

    Изменено: Navigation Blade - добавлена адаптивность (под мобильные устройства) и иконка юзера, удалено стандартное меню

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,19 +1,19 @@
-<nav class="bg-white border-b border-secondary {{ Route::is('dashboard') ? 'position-absolute top-0' : '' }} w-100" {{ Route::is('dashboard') ? 'style="height: 55px;"' : '' }}>
+<nav class="{{ Route::is('dashboard') ? 'bg-transparent' : 'bg-white border-b' }}  border-secondary {{ Route::is('dashboard') ? 'position-absolute top-0' : '' }} w-100" {{ Route::is('dashboard') ? 'style=height:55px' : '' }}>
     <!-- Primary Navigation Menu -->
     <div class="mx-auto px-4 sm:px-6 lg:px-8 py-1">
         <div class="flex justify-between py-2">
-            <div class="flex">
+            <div class="flex me-3">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     @if (isset($back_href) and isset($back_title))
-                        <a href="{{ $back_href }}" class="text-decoration-none fs-5">
+                        <a href="{{ $back_href }}" class="text-decoration-none fs-5 dashboard_link">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="d-inline-block bi bi-chevron-left" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                             </svg>
                             <span class="align-middle">{{ $back_title }}</span>
                         </a>
                     @else
-                        <h4 class="fw-bold mb-0">Ferone</h4>
+                        <h4 class="{{ Route::is('dashboard') ? 'text-white' : '' }} fw-bold mb-0">Ferone</h4>
                     @endif
                 </div>
 
@@ -24,14 +24,23 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out fs-5 text-primary">
-                            <div>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</div>
+                            <div class="{{ Route::is('dashboard') ? 'text-white' : '' }} me-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                                </svg>
+                            </div>
+
+                            <div class="{{ Route::is('dashboard') ? 'text-white' : '' }} dashboard_link">
+                                {{ Auth::user()->full_name }}
+                            </div>
 
                             <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <svg class="fill-current h-4 w-4 {{ Route::is('dashboard') ? 'text-white' : '' }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
@@ -39,7 +48,7 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.index')" class="text-center text-decoration-none">
+                        <x-dropdown-link :href="route('profile.index')" class="text-right text-decoration-none">
                             {{ __('Профиль') }}
                         </x-dropdown-link>
 
@@ -47,22 +56,12 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="text-center text-decoration-none">
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="text-right text-decoration-none">
                                 {{ __('Выйти из системы') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
             </div>
         </div>
     </div>
