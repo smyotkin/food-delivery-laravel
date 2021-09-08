6d47491810c52cffcc53a5f942d0189b0fe81b7f
smyotkin <smyotkin@gmail.com>
2021-09-08 10:01:44 +0300

    Изменено: Positions Blade - шапка с поиском в отдельном шаблон и оптимизирован JS новой функцией updateTableList

--- resources/views/users/positions.blade.php
+++ resources/views/users/positions.blade.php
@@ -9,16 +9,7 @@
     @include('layouts.sub-navigation')
 
     <div class="container-fluid px-4 px-md-5 mb-5">
-        <div class="row">
-            <div class="col-12 col-md-5 mt-4">
-                <input type="text" id="name_slug-search" class="form-control rounded-0" placeholder="Поиск по названию или метке" aria-label="Поиск по названию или метке">
-            </div>
-            <div class="col-auto mt-4 d-flex align-items-center">
-                <div class="spinner-border text-secondary d-none" role="status" id="preloader">
-                    <span class="sr-only">Загрузка...</span>
-                </div>
-            </div>
-        </div>
+        @include('components.header-search-preloader', ['id' => 'positions-search', 'placeholder' => 'Поиск по названию или метке'])
 
         <div class="row mb-3 mt-md-5">
             <div class="col-12 col-md-auto lh-1">
@@ -51,68 +42,8 @@
     </div>
 
     <script>
-        let userSearch = $('#name_slug-search');
-        let usersBlock = $('#positions_ajax');
-        let usersTable = $('#positions_ajax > .row');
-        let ajaxSearchDelay = 300;
-        let usersUpdateDelay = 10000;
-        let getSearchCookie = getCookie('positions_query_str');
-        let page = 1;
-        let searchNow = false;
-
         $(document).ready(function() {
-            if (getSearchCookie) {
-                userSearch.val(getSearchCookie);
-            }
-
-            showPositions();
-
-            userSearch.on('keyup', function () {
-                document.cookie = 'positions_query_str=' + encodeURIComponent($(this).val());
-
-                if ($(this).val().length >= 0) {
-                    searchNow = true;
-
-                    showPositions();
-                } else {
-                    searchNow = false;
-                }
-            });
-
-            $(document).on('click', '.table_pagination a', function(event) {
-                event.preventDefault();
-
-                searchNow = false;
-                page = $(this).attr('href').split('page=')[1];
-
-                showPositions(page);
-            });
+            updateTableList('positions', '{{ route('positions.getAjax') }}');
         });
-
-        function showPositions(page = 1) {
-            $.ajax({
-                type: 'GET',
-                data: {
-                    page: page,
-                    query: userSearch.val(),
-                },
-                url: '{{ route('positions.getAjax') }}',
-                beforeSend: function () {
-                    $('#preloader').removeClass('d-none');
-                },
-                complete: function() {
-                    $('#preloader').addClass('d-none');
-                },
-                success: function (data) {
-                    usersBlock.find('.table_pagination').remove();
-
-                    if (searchNow || page == 1) {
-                        usersBlock.html(data);
-                    } else {
-                        usersBlock.append(data);
-                    }
-                }
-            });
-        }
     </script>
 </x-app-layout>
