ea795e8c91a268790938c5ff8a9d359f2c0d78bb
smyotkin <smyotkin@gmail.com>
2021-09-08 19:56:51 +0300

    Изменено: Custom Config - добавлен параметр send_sms в корень конфига

--- config/custom.php
+++ config/custom.php
@@ -1,11 +1,14 @@
 <?php
 
 return [
+    'send_sms' => 0,
+
     'subdomain' => [
         'api' => 'api.ferone-laravel.ru',
         'ferone' => 'back.ferone-laravel.ru',
         'site' => 'suare-laravel.su',
     ],
+
     'password_resets' => [
         'attempts_per_day' => 10,
         'pin_lifetime_minutes' => 5,
