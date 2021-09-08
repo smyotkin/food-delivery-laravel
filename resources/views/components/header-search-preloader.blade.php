02d9cc6a054d9f3a420853f2995ee6af3989d6d3
smyotkin <smyotkin@gmail.com>
2021-09-08 09:56:49 +0300

    Добавлено: Header Search Preloader Blade - отдельный шаблон для шапки с поиском и прелоадером

---
+++ resources/views/components/header-search-preloader.blade.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-12 col-md-5 mt-4">
+        <input type="text" id="{{ $id }}" class="form-control rounded-0" placeholder="{{ $placeholder }}" aria-label="{{ $placeholder }}">
+    </div>
+
+    <div class="col-auto mt-4 d-flex align-items-center">
+        @include('components.preloader')
+    </div>
+</div>
