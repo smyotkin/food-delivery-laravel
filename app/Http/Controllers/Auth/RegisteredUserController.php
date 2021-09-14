04806892acb464c59070db95a0065eb84f3b5133
smyotkin <smyotkin@gmail.com>
2021-09-14 08:26:14 +0300

    Изменено: проведен рефакторинг стандартных контроллеров (изменены типы возвращаемых данных)

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -16,7 +16,7 @@ class RegisteredUserController extends Controller
     /**
      * Display the registration view.
      *
-     * @return \Illuminate\View\View
+     * @return mixed
      */
     public function create()
     {
