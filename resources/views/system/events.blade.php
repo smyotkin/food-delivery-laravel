79b368fa4d4382b99e7a584a0b4a2a80e9d65e0c
smyotkin <smyotkin@gmail.com>
2021-09-14 09:07:41 +0300

    Изменено: Events Blade - изменены маршруты, оптимизирован JS, и сокращен шаблон путем добавления модуля поиска(в шапке)

--- resources/views/system/events.blade.php
+++ resources/views/system/events.blade.php
@@ -9,16 +9,7 @@
     @include('layouts.settings-navigation')
 
     <div class="container-fluid px-4 px-md-5 mb-5">
-        <div class="row">
-            <div class="col-12 col-md-5 mt-4">
-                <input type="text" id="label_msg-search" class="form-control rounded-0" placeholder="Поиск по метке или сообщению" aria-label="Поиск по метке или сообщению">
-            </div>
-            <div class="col-auto mt-4 d-flex align-items-center">
-                <div class="spinner-border text-secondary d-none" role="status" id="preloader">
-                    <span class="sr-only">Загрузка...</span>
-                </div>
-            </div>
-        </div>
+        @include('components.header-search-preloader', ['id' => 'events-search', 'placeholder' => 'Поиск по метке или сообщению'])
 
         <div class="row mb-3 mt-md-5">
             <div class="col-auto lh-1">
@@ -70,67 +61,8 @@
     </div>
 
     <script>
-        let search = $('#label_msg-search');
-        let ajaxBlock = $('#events_ajax');
-        let getSearchCookie = getCookie('events_query_str');
-        let page = 1;
-        let searchNow = false;
-        let listCount = 0;
-
         $(document).ready(function() {
-            if (getSearchCookie)
-                search.val(getSearchCookie);
-
-            showList();
-
-            search.on('keyup', function () {
-                document.cookie = 'events_query_str=' + encodeURIComponent($(this).val());
-                if ($(this).val().length >= 0) {
-                    searchNow = true;
-                    showList();
-                } else {
-                    searchNow = false;
-                }
-            });
-
-        });
-
-        function showList(page = 1) {
-            let query = search.val();
-
-            $('#download_csv').attr('href', '{{ '/system/events/export.csv' }}' + '?query=' + query);
-
-            $.ajax({
-                type: 'GET',
-                data: {
-                    query: query,
-                },
-                url: '{{ route('system/events/get.ajax') }}?page=' + page,
-                beforeSend: function () {
-                    $('#preloader').removeClass('d-none');
-                },
-                complete: function() {
-                    $('#preloader').addClass('d-none');
-                },
-                success: function (data) {
-                    $('.table_pagination', ajaxBlock).remove();
-
-                    if (searchNow || page == 1) {
-                        ajaxBlock.html(data);
-                    } else {
-                        ajaxBlock.append(data);
-                    }
-                }
-            });
-        }
-
-        $('body').on('click', '.table_pagination a', function(event) {
-            event.preventDefault();
-
-            searchNow = false;
-            page = $(this).attr('href').split('page=')[1];
-
-            showList(page);
+            updateTableList('events', '{{ route('system/events/get.ajax') }}');
         });
 
         $('body').on('click', '.clear_events', function(event) {
@@ -158,14 +90,14 @@
                             period: routeParams.get('period'),
                         },
                         success: function (data) {
-                            if (JSON.parse(data).success) {
-                                showList();
-                            }
+                            updateTableList('events', '{{ route('system/events/get.ajax') }}');
+                            let dataCount = data.count;
+                            let viewCount = $('#clearEvents .count');
 
-                            if (JSON.parse(data).count) {
-                                let count = parseInt($('#clearEvents .count').text()) - parseInt(JSON.parse(data).count);
+                            if (dataCount) {
+                                let count = parseInt(viewCount.text()) - parseInt(dataCount);
 
-                                $('#clearEvents .count').text(count);
+                                viewCount.text(count);
 
                                 if (count <= 0) {
                                     $('#clearEvents').hide();
@@ -173,14 +105,23 @@
 
                                 Swal.fire({
                                     title: 'Успешно',
-                                    text: 'Всего было удалено: ' + JSON.parse(data).count,
+                                    text: 'Всего было удалено: ' + dataCount,
                                     icon: 'info',
+                                    showConfirmButton: false,
+                                });
+                            } else {
+                                Swal.fire({
+                                    title: 'Внимание!',
+                                    text: 'Нет данных',
+                                    icon: 'warning',
+                                    cancelButtonText: 'Закрыть',
+                                    showCancelButton: true,
                                     showConfirmButton: false,
                                 });
                             }
                         },
                         error: function (response) {
-                            if (response.status === 404) {
+                            if (response.responseJSON.message) {
                                 Swal.fire({
                                     title: 'Внимание!',
                                     text: response.responseJSON.message,
