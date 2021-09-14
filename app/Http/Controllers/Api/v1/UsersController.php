df8a7aea33bd90ac9c6f1c10cf9a6ab97ecf7530
smyotkin <smyotkin@gmail.com>
2021-09-14 08:39:55 +0300

    Изменено: Users Controller - изменен PHPDOC

--- app/Http/Controllers/Api/v1/UsersController.php
+++ app/Http/Controllers/Api/v1/UsersController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UsersResource;
 use App\Services\UsersService;
-use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class UsersController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
@@ -22,21 +22,11 @@ class UsersController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-//     * @return void
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param int $id
      * @return UsersResource
+     * @throws \Throwable
      */
     public function show($id)
     {
@@ -45,28 +35,5 @@ class UsersController extends Controller
                 'id' => $id,
             ])
         );
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-//     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-//     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
