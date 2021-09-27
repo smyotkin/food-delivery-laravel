deaf2fbd41f0fd03daab01f56943b71e9df69970
smyotkin <smyotkin@gmail.com>
2021-09-27 21:10:42 +0300

    Добавлено: Cities Model - модель таблицы городов

---
+++ app/Models/Cities.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder query()
+ * @method static \Illuminate\Database\Eloquent\Builder whereCityId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereLastPage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereLastSeen($value)
+ * @method static \Illuminate\Database\Eloquent\Builder wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder wherePositionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Cities extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'folder',
+        'timezone',
+        'work_hours',
+        'work_hours_shift',
+        'is_active',
+    ];
+
+    protected $appends = [
+        'phone_formatted',
+    ];
+
+    public function getPhoneFormattedAttribute()
+    {
+        return "+7 {$this->phone}";
+    }
+
+    public function getWorkHoursArrayAttribute()
+    {
+        return json_decode($this->work_hours, true);
+    }
+}
