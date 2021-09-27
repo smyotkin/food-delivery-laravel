64c0bb3d9055cdf480c1e34bc86e040809377c33
smyotkin <smyotkin@gmail.com>
2021-09-27 21:21:02 +0300

    Добавлено: DaData - добавлена библиотека с конфигом

---
+++ config/dadata.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'token' => env('DADATA_TOKEN', 'e834eaed8e896cc14ccff6dc9bae39fcb8f2dbdd'),
+    'secret' => env('DADATA_SECRET', 'ee903ad1610fe6576dda45c9f4142415339f4796'),
+];
