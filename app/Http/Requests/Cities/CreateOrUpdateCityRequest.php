0ecf2454daa61913e6157d75ad0bad9b580ac5db
smyotkin <smyotkin@gmail.com>
2021-10-06 17:24:25 +0300

    Изменено: Create Or Update City Request - добавлены новые параметры валидации

--- app/Http/Requests/Cities/CreateOrUpdateCityRequest.php
+++ app/Http/Requests/Cities/CreateOrUpdateCityRequest.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Requests\Cities;
 
+use App\Services\UsersService;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CreateOrUpdateCityRequest extends FormRequest
 {
+    /**
+     * Обработка перед валидацией
+     *
+     * @return void
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'multicode' => !empty($this->multicode) ? 1 : 0,
+            'is_active' => !empty($this->is_active) ? 1 : 0,
+        ]);
+    }
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -28,8 +42,12 @@ class CreateOrUpdateCityRequest extends FormRequest
             'name' => 'required|string|min:2|max:255',
             'phone' => 'string|max:50',
             'folder' => 'string|max:255',
+            'multicode' => 'boolean',
+            'is_active' => 'boolean',
+            'timezone' => 'string|in:' . implode(',', array_keys(UsersService::timezones)),
             'work_hours' => 'json',
             'work_hours_shift' => 'integer|in:-30,-25,-20,-15,-10,-5,0,5,10,15,20,25,30',
+            'kladr_cities' => 'json',
         ]);
 
         switch ($this->method()) {
