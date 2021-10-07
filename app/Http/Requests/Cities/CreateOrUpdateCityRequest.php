b8a18c9e0c2aa2ad8f34efe4f8c33e47deb8f8e6
smyotkin <smyotkin@gmail.com>
2021-10-07 11:21:34 +0300

    Изменено: Create Or Update City Request - проставлены правильные правила валидации

--- app/Http/Requests/Cities/CreateOrUpdateCityRequest.php
+++ app/Http/Requests/Cities/CreateOrUpdateCityRequest.php
@@ -17,6 +17,8 @@ class CreateOrUpdateCityRequest extends FormRequest
         $this->merge([
             'multicode' => !empty($this->multicode) ? 1 : 0,
             'is_active' => !empty($this->is_active) ? 1 : 0,
+            'work_hours' => !empty($this->work_hours) && is_array($this->work_hours) ? json_encode($this->work_hours) : null,
+            'folder' => '/' . strtolower($this->folder),
         ]);
     }
 
@@ -40,13 +42,13 @@ class CreateOrUpdateCityRequest extends FormRequest
         $rules = collect([
             'id' => 'required|integer|exists:cities,id',
             'name' => 'required|string|min:2|max:255',
-            'phone' => 'string|max:50',
-            'folder' => 'string|max:255',
+            'phone' => 'required|string|max:50',
+            'folder' => 'string|max:255|unique:cities,folder,' . $this->folder,
             'multicode' => 'boolean',
             'is_active' => 'boolean',
-            'timezone' => 'string|in:' . implode(',', array_keys(UsersService::timezones)),
-            'work_hours' => 'json',
-            'work_hours_shift' => 'integer|in:-30,-25,-20,-15,-10,-5,0,5,10,15,20,25,30',
+            'timezone' => 'required|string|in:' . implode(',', array_keys(UsersService::timezones)),
+            'work_hours' => 'required|json',
+            'work_hours_shift' => 'required|integer|in:-30,-25,-20,-15,-10,-5,0,5,10,15,20,25,30',
             'kladr_cities' => 'json',
         ]);
 
