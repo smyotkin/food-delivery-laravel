6cd800e18303c54a1603a16cd373ee36baac96f0
smyotkin <smyotkin@gmail.com>
2021-10-07 11:16:38 +0300

    Добавлено: миграция для Cities - делаем колонку folder uniq

---
+++ database/migrations/2021_10_07_104834_add_uniq_folder_column_into_cities.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUniqFolderColumnIntoCities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->string('folder', 255)->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            //
+        });
+    }
+}
